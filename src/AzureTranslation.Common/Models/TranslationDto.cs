baecbb74f1d01603ae67d8d3d77f888717480d1a
Mario Ramos <mariodarken@gmail.com>
2025-03-08 00:03:40 +0100

    Update TranslationDto.cs

--- src/AzureTranslation.Common/Models/TranslationDto.cs
+++ src/AzureTranslation.Common/Models/TranslationDto.cs
@@ -1,15 +1,25 @@
-﻿using AzureTranslation.Common.Enums;
+﻿using System.Text.Json.Serialization;
+
+using AzureTranslation.Common.Enums;
 
 namespace AzureTranslation.Common.Models;
 
 public class TranslationDto
 {
     public string Id { get; set; }
+
     public string OriginalText { get; set; }
+
     public string TranslatedText { get; set; }
+
     public string DetectedLanguage { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public TranslationStatus Status { get; set; }
+
     public string ErrorMessage { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public DateTime? CompletedAt { get; set; }
 }
